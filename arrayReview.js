69c1c27423a36a058442bddfc43aea386fbfefbe
Casey Arnold <caseyarnold89@gmail.com>
2016-01-30 10:21:02 -0700

    First three array problems done

--- arrayReview.js
+++ arrayReview.js
@@ -3,7 +3,12 @@ var threeItems = [1,2,3];
 //alert the result of your function
 
   //code here
+function last(arr) {
+    var lastItem = arr[arr.length-1];
+    alert(lastItem);
+}
 
+last(threeItems);
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
@@ -13,20 +18,46 @@ var threeItems = [1,2,3];
 //Loop through evenArray removing all values that aren't even 
 var evenArray = [1,2,3,6,22,98,45,23,22,12];
 
-  //code here
 
+function removeOdd(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i]%2 !== 0) {
+            arr.splice(i,1);
+            i--;
+        }
+    }
+}
+
+removeOdd(evenArray);
+console.log(evenArray);
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
-//below you're given a function that will return a random number between 0 and 30 and an array full or numbers 'randomArray'. Your job is to write a function that will get a random number, then loop through the array to see if that random number is in the array. If it is, alert true, if it's not, alert false
+//below you're given a function that will return a random number between 0 and 30 and an array full 
+//or numbers 'randomArray'. 
+//Your job is to write a function that will get a random number, then loop through the array to see 
+//if that random number is in the array. If it is, alert true, if it's not, alert false
 var getRandomArbitrary = function() {
   return Math.floor(Math.random() * (30 - 0) + 0);
 }
 var randomArray = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 
-  //code here
+function checkRand(arr) {
+    var flag = false;
+    var num = getRandomArbitrary();
+    console.log(num);
+    for (var i in arr) {
+        if (arr[i] === num) {
+            flag = true;
+            return flag;
+        }
+    }
+    return flag;
+}
+
+checkRand(randomArray);
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
