3ef09db63dd03b8a8954777e6483ef00fe591bc5
Casey Arnold <caseyarnold89@gmail.com>
2016-01-30 12:18:02 -0700

    last problem of objectReview

--- objectReview.js
+++ objectReview.js
@@ -1,18 +1,33 @@
-//Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category. 
+//Make a 'favoriteThings' object that contains the following keys: band, food, person, 
+//book, movie, holiday. Have the values to those keys be your favorite thing in that 
+//category. 
 
-  //Your code here
+var favoriteThings = {
+    band: 'Yeasayer',
+    food: 'Mexican',
+    person: 'Lacey',
+    book: 'Mans Search For Meaning',
+    movie: 'Seven Pounds',
+    holiday: 'Christmas'
+};
 
-//After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
+//After you've made your object, add another key named 'car' with the value being 
+//your favorite car and then another key named 'brand' with the value being your 
+//favorite brand.
 
-  //Your code here
+favoriteThings.car = 'Toyota 4Runner';
+favoriteThings.brand = 'Backcountry';
 
-//Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'. 
+//Now change the food key in your favoriteThings object to be 'Lettuce' and change 
+//the book key in your favoriteThings object to be '50 Shades of Gray'. 
 
-  //Your code here
+favoriteThings.food = 'Lettuce';
+favoriteThings.book = '50 Shades of Gray';
 
 //Now, alert your favorite person, then alert your favorite book.
 
-  //Your code here
+alert(favoriteThings.person);
+alert(favoriteThings.book);
 
 
 
@@ -29,17 +44,30 @@ var user = {
     age: 0
 }
 
-//Above you're given a user object. Loop through the user object checking to make sure that each value is truthy. If it's not truthy, remove it from the object. hint: 'delete'.
+//Above you're given a user object. Loop through the user object checking to make sure
+// that each value is truthy. If it's not truthy, remove it from the object. hint: 
+//'delete'.
 
-  //Your code here
+for (var i in user) {
+    if (!user[i]) {
+        delete user[i];
+    }
+}
 
-//Once you get your truthy object, Change the remaining values in the object to be specific to you (name: 'your name', username: 'your username'), rather than my information.
+console.log(user.name);
+console.log(user);
+//Once you get your truthy object, Change the remaining values in the object to be 
+//specific to you (name: 'your name', username: 'your username'), rather than my 
+//information.
 
-  //Your code here
+user.name = 'Casey Arnold';
+user.pwHash ='blahBlah';
+user.username = 'caseyarnold';
+
 
 //Now console.log your object and make sure it looks right.
 
-  //Your code here
+console.log(user);
 
 
 
